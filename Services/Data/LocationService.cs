4d7d2989b3f31a488ba6526b5243096481b7c6d7
Chris Wilson <cwilson@xaipete.net>
2019-08-10 14:32:08 -0500

    Added test code for geographically bounded search

--- Services/Data/LocationService.cs
+++ Services/Data/LocationService.cs
@@ -4,8 +4,10 @@ using Microsoft.Extensions.Logging;
 using MkeAlerts.Web.Data;
 using MkeAlerts.Web.Models.Data.Accounts;
 using MkeAlerts.Web.Models.Data.Places;
+using NetTopologySuite.Geometries;
 using System.Linq;
 using System.Threading.Tasks;
+using Coordinate = GeoAPI.Geometries.Coordinate;
 
 namespace MkeAlerts.Web.Services.Data
 {
@@ -22,7 +24,26 @@ namespace MkeAlerts.Web.Services.Data
 
         protected override async Task<IQueryable<Location>> ApplyReadSecurity(ApplicationUser applicationUser, IQueryable<Location> queryable)
         {
-            return queryable;
+            //return queryable;
+            Point myLocation = new Point(-87.885001526424077, 43.06512940428771)
+            {
+                SRID = 4326
+            };
+
+            Polygon bounds = new Polygon(new LinearRing(new Coordinate[]
+            {
+                new Coordinate(-87.88540495881335, 43.06530806130551), // NW
+                new Coordinate(-87.88540495881335, 43.06381877978135), // SW
+                new Coordinate(-87.88293330320613, 43.06381877978135), // SE
+                new Coordinate(-87.88293330320613, 43.06530806130551), // NE
+                new Coordinate(-87.88540495881335, 43.06530806130551), // NW
+            }));
+            bounds.SRID = 4326;
+            //ne: { lat: 43.06381877978135, lng: -87.88540495881335}
+            //sw: { lat: 43.06530806130551, lng: -87.88293330320613}
+            //__proto__: Object
+
+            return queryable.Where(x => x.Outline.Overlaps(bounds));
         }
 
         protected override async Task<bool> CanWrite(ApplicationUser applicationUser, Location dataModel)
