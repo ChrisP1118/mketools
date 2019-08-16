b94a9dbbb2b53349c8f70d5762cab2ccb8f7bb3b
Chris Wilson <cwilson@xaipete.net>
2019-08-16 13:51:52 -0500

    Added data load on home page map

--- ClientApp/src/components/Home.vue
+++ ClientApp/src/components/Home.vue
@@ -31,7 +31,7 @@
               </b-form>
             </b-col>
           </b-row>
-          <p><i>Remember! An increased awareness of crime does not mean crime is actually increasing.</i></p>
+          <p><i>Remember! An increased awareness of crime does not necessarily indicate an increase in crime.</i></p>
         </b-jumbotron>
       </b-col>
     </b-row>
@@ -59,6 +59,7 @@
                 </b-form-group>
                 <b-form-group>
                   <b-button type="submit" variant="primary">Go</b-button>
+                  <b-button type="button" @click="onClear">Clear</b-button>
                 </b-form-group>
               </b-form-row>
             </b-form>
@@ -107,7 +108,10 @@ export default {
       streetNames: [],
       streetTypes: [],
       google: null,
-      map: null
+      map: null,
+      bounds: null,
+      tabIndex: 0,
+      markerWrappers: []
     }
   },
   methods: {
@@ -138,18 +142,127 @@ export default {
           console.log(error);
         });
     },
+    onClear: function () {
+      console.log('clear!');
+      this.showJumbotron = true;
+      this.number = null;
+      this.streetDirection = '';
+      this.streetName = '';
+      this.streetType = '';
+    },
     onTabChanged: function(tabIndex) {
-      console.log('Tab changed: ' + tabIndex);
+      this.tabIndex = tabIndex;
+      this.updateTab();
+    },
+    updateTab: function () {
+      if (this.tabIndex == 0) {
+        this.loadActiveCalls();
+      }
+    },
+    loadActiveCalls: function () {
+      axios
+        .get('/api/DispatchCall?offset=0&limit=100&order=ReportedDateTime%20desc&filter=Status%20%3D%20%22Service%20in%20Progress%22' + this.getBoundsFilter())
+        .then(response => {
+          console.log(response.data);
+          this.drawMarkers(response.data, (i) => { return i.Geometry; }, (i) => { return i.CallNumber; }, (i) => { return i.NatureOfCall; });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    drawMarkers: function (items, getItemMarkerGeometry, getItemId, getItemInfoWindowText) {
+      if (!google)
+        return;
+
+      let newMarkerWrappers = [];
+
+      items.forEach(i => {
+
+        let geometry = getItemMarkerGeometry(i);
+
+        if (!geometry)
+          return;
+
+        let existingMarkerWrapper = this.markerWrappers.find(w => w.id == getItemId(i));
+        if (existingMarkerWrapper) {
+          newMarkerWrappers.push(existingMarkerWrapper);
+          return;
+        } else {
+
+          let point = geometry.coordinates[0][0];
+
+          let marker = new google.maps.Marker({
+            position: {
+              lat: point[1],
+              lng: point[0]
+            },
+            map: this.map
+          });
+
+          marker.addListener('click', e => {
+            if (this.openInfoWindow)
+              this.openInfoWindow.close();
+
+            this.openInfoWindow = new google.maps.InfoWindow({
+              content: getItemInfoWindowText(i)
+            });
+            this.openInfoWindow.open(this.map, marker);
+          });
+
+          newMarkerWrappers.push({
+            id: getItemId(i),
+            marker: marker
+          });
+        }
+      });
+
+      let oldMarkerWrappers = this.markerWrappers.filter(m => { return !(newMarkerWrappers.find(x => x.id == m.id)); });
+      oldMarkerWrappers.forEach(w => { 
+        w.marker.setMap(null);
+      });
+      this.markerWrappers = newMarkerWrappers;
+    },
+    getBoundsFilter: function() {
+      if (!this.bounds)
+        return '';
+
+      return '&northBound=' + this.bounds.ne.lat + '&southBound=' + this.bounds.sw.lat + '&eastBound=' + this.bounds.ne.lng + '&westBound=' + this.bounds.sw.lng;
     }
   },
   async mounted () {
     this.loadStreetReferences();
-      this.google = await gmapsInit();
-      this.map = new google.maps.Map(document.getElementById('homeMap'), {
-        center: { lat: 43.0315528, lng: -87.9730566 },
-        zoom: 10,
-        gestureHandling: 'greedy'
-      });
+
+    this.google = await gmapsInit();
+    this.map = new google.maps.Map(document.getElementById('homeMap'), {
+      center: { lat: 43.0315528, lng: -87.9730566 },
+      zoom: 10,
+      gestureHandling: 'greedy'
+    });
+
+    let boundsChangedTimeout = null;
+
+    this.map.addListener('bounds_changed', e => {
+
+      if (boundsChangedTimeout != null)
+        clearTimeout(boundsChangedTimeout);
+
+      boundsChangedTimeout = setTimeout(() => {
+        let bounds = this.map.getBounds();
+        this.bounds = {
+          ne: {
+            lat: bounds.na.j,
+            lng: bounds.ga.j
+          },
+          sw: {
+            lat: bounds.na.l,
+            lng: bounds.ga.l
+          }
+        };
+
+        this.updateTab();
+
+      }, 1000);
+    });      
   }
 };
 </script>
