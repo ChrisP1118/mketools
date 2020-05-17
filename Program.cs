dfe2d5a878266e384434c431299667a9f22df3dd
Chris Wilson <cwilson@xaipete.net>
2020-05-17 17:41:53 -0500

    Removed logging to files

--- Program.cs
+++ Program.cs
@@ -20,63 +20,58 @@ namespace MkeAlerts.Web
         {
             Log.Logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
-                //.WriteTo.Console()
-                //.WriteTo.File("logs/log.txt",
-                //    rollingInterval: RollingInterval.Day,
-                //    retainedFileCountLimit: 14,
-                //    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {SourceContext:l} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
                 .WriteTo.Logger(l => l
                     .MinimumLevel.Information()
                     .Filter.ByIncludingOnly(Matching.FromSource("Microsoft.AspNetCore.Hosting"))
                     .WriteTo.Console()
                 )
-                .WriteTo.Logger(l => l
-                    .MinimumLevel.Information()
-                    .Filter.ByIncludingOnly(Matching.FromSource("MkeAlerts"))
-                    .WriteTo.File("logs/MkeAlerts.txt",
-                        rollingInterval: RollingInterval.Day,
-                        retainedFileCountLimit: 14
-                    )
-                )
-                .WriteTo.Logger(l => l
-                    .MinimumLevel.Information()
-                    .Filter.ByIncludingOnly(Matching.FromSource("Microsoft.AspNetCore.Hosting"))
-                    .WriteTo.File("logs/Web.txt",
-                        rollingInterval: RollingInterval.Day,
-                        retainedFileCountLimit: 14
-                    )
-                )
+                //.WriteTo.Logger(l => l
+                //    .MinimumLevel.Information()
+                //    .Filter.ByIncludingOnly(Matching.FromSource("MkeAlerts"))
+                //    .WriteTo.File("logs/MkeAlerts.txt",
+                //        rollingInterval: RollingInterval.Day,
+                //        retainedFileCountLimit: 14
+                //    )
+                //)
+                //.WriteTo.Logger(l => l
+                //    .MinimumLevel.Information()
+                //    .Filter.ByIncludingOnly(Matching.FromSource("Microsoft.AspNetCore.Hosting"))
+                //    .WriteTo.File("logs/Web.txt",
+                //        rollingInterval: RollingInterval.Day,
+                //        retainedFileCountLimit: 14
+                //    )
+                //)
                 .WriteTo.Logger(l => l
                     .MinimumLevel.Information()
                     .Filter.ByIncludingOnly(Matching.FromSource("MkeAlerts"))
                     .WriteTo.Seq("http://localhost:5341")
-                    .WriteTo.File(
-                        new RenderedCompactJsonFormatter(),
-                        "logs/MkeAlerts.json",
-                        rollingInterval: RollingInterval.Day,
-                        retainedFileCountLimit: 14
-                    )
+                    //.WriteTo.File(
+                    //    new RenderedCompactJsonFormatter(),
+                    //    "logs/MkeAlerts.json",
+                    //    rollingInterval: RollingInterval.Day,
+                    //    retainedFileCountLimit: 14
+                    //)
                 )
                 .WriteTo.Logger(l => l
                     .MinimumLevel.Information()
                     .Filter.ByIncludingOnly(Matching.FromSource("Microsoft.AspNetCore.Hosting"))
                     .WriteTo.Seq("http://localhost:5341")
-                    .WriteTo.File(
-                        new RenderedCompactJsonFormatter(),
-                        "logs/Web.json",
-                        rollingInterval: RollingInterval.Day,
-                        retainedFileCountLimit: 14
-                    )
+                    //.WriteTo.File(
+                    //    new RenderedCompactJsonFormatter(),
+                    //    "logs/Web.json",
+                    //    rollingInterval: RollingInterval.Day,
+                    //    retainedFileCountLimit: 14
+                    //)
                 )
                 .WriteTo.Logger(l => l
                     .MinimumLevel.Warning()
                     .WriteTo.Seq("http://localhost:5341")
-                    .WriteTo.File(
-                        new RenderedCompactJsonFormatter(),
-                        "logs/Warnings.json",
-                        rollingInterval: RollingInterval.Day,
-                        retainedFileCountLimit: 14
-                    )
+                    //.WriteTo.File(
+                    //    new RenderedCompactJsonFormatter(),
+                    //    "logs/Warnings.json",
+                    //    rollingInterval: RollingInterval.Day,
+                    //    retainedFileCountLimit: 14
+                    //)
                 )
                 //.WriteTo.Logger(l => l
                 //    .MinimumLevel.Warning()
@@ -100,14 +95,11 @@ namespace MkeAlerts.Web
                 //        retainedFileCountLimit: 14
                 //    )
                 //)
-                //<logger name=".*" minlevel="Warn" writeTo="EF" final="true" />
-                //<logger name="Hangfire.*" minlevel="Info" writeTo="Hangfire" final="true" />
                 .CreateLogger();
 
             try
             {
                 Log.Information("Starting up");
-                //logger.Debug("Starting app");
                 CreateWebHostBuilder(args).Build().Run();
             }
             catch (Exception ex)
