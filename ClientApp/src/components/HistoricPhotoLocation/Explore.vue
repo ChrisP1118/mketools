2e85c7b22bc09db22035896d94544f7db8ce6dfc
Chris Wilson <cwilson@xaipete.net>
2020-08-25 21:45:46 -0500

    Lots of UI tweaks to historic photos

--- ClientApp/src/components/HistoricPhotoLocation/Explore.vue
+++ ClientApp/src/components/HistoricPhotoLocation/Explore.vue
@@ -3,47 +3,58 @@
     <page-title title="Historic Photos" />
     <b-row class="mb-3">
       <b-col>
+        <b-card bg-variant="light" class="mb-3">
+          <b-card-text class="text-center">
+            This page lets you explore historic photos from the <a href="http://www.mpl.org/special_collections/images/index.php?slug=milwaukee-historic-photos" target="_blank">
+            Milwaukee Public Library's historic photos collection</a>.
+          </b-card-text>
+        </b-card>
         <b-card bg-variant="light">
           <b-card-text>
             <address-lookup :addressData.sync="addressData" :locationData.sync="locationData" />
           </b-card-text>
         </b-card>        
-        <p class="small">This list contains historic photos from the <a href="http://www.mpl.org/special_collections/images/index.php?slug=milwaukee-historic-photos" target="_blank">Milwaukee
-          Public Library's historic photos collection</a>.
-        </p>
       </b-col>
     </b-row>
     <b-row>
-      <b-col>
-        <div style="height: 80vh; width: 100%;">
-          <l-map style="height: 100%; width: 100%" :zoom="zoom" :center="center" @update:zoom="zoomUpdated" @update:center="centerUpdated" @update:bounds="boundsUpdated">
+      <b-col lg="4" xl="6" class="mb-3">
+        <div class="explore-map-wrapper">
+          <l-map class="explore-map" :zoom="zoom" :center="center" @update:zoom="zoomUpdated" @update:center="centerUpdated" @update:bounds="boundsUpdated" @ready="refreshItems" :max-bounds="maxBounds">
             <l-tile-layer :url="tileUrl" :attribution="attribution"></l-tile-layer>
             <l-marker v-for="marker in markers" v-bind:key="marker.id" :lat-lng="marker.position" :icon="marker.icon" @click="onMarkerClick(marker.id)">
             </l-marker>
           </l-map>
         </div>
       </b-col>
-      <b-col>
+      <b-col lg="8" xl="6">
         <div v-if="!selectedItem">
+          <p>Click on a marker on the map to view photos from that location.</p>
         </div>
         <div v-if="selectedItem">
           <b-card-group columns>
-            <b-card v-for="(historicPhoto, index) in selectedItem.historicPhotos" v-bind:key="index" :img-src="historicPhoto.imageUrl">
+            <b-card v-for="(historicPhoto, index) in selectedItem.historicPhotos" v-bind:key="index">
               <template v-slot:header>
-                <h4>{{historicPhoto.title}}</h4>
+                <h4><a :href="historicPhoto.url" target="_blank">{{historicPhoto.title}}</a></h4>
               </template>
-                <b-card-title>{{historicPhoto.place}}</b-card-title>
-                <b-card-sub-title>{{historicPhoto.currentAddress}}</b-card-sub-title>
-                <b-card-text>
-                  {{historicPhoto.description}}
-                  {{historicPhoto.year}}
-                </b-card-text>
-                <a :href="historicPhoto.url" target="_blank">View Detail</a>
+              <b-card-img :src="historicPhoto.imageUrl" @click="onThumbnailClick(historicPhoto)"></b-card-img>
+              <b-card-title>{{historicPhoto.place}}</b-card-title>
+              <b-card-sub-title>{{historicPhoto.currentAddress}}</b-card-sub-title>
+              <b-card-text>
+                {{historicPhoto.description}}
+                <p v-if="historicPhoto.date">
+                  <em>{{historicPhoto.date}}</em>
+                </p>
+              </b-card-text>
             </b-card>
           </b-card-group>
         </div>
       </b-col>
     </b-row>
+    <b-modal id="modalFullSizeImage" size="lg" scrollable :title="selectedHistoricPhoto ? selectedHistoricPhoto.title : ''">
+      <div v-if="selectedHistoricPhoto" class="text-center">
+        <b-img :src="selectedHistoricPhoto.imageUrl"></b-img>
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -61,23 +72,42 @@ export default {
       tileUrl: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
       zoom: 17,
       center: [43.0417177, -87.9098005],
-      bounds: null,
+      bounds: {
+        _northEast: {
+          lat: 43.04344109368516,
+          lng: -87.90466894743822
+        },
+        _southWest: {
+          lat: 43.04001447050734,
+          lng: -87.91456093428515
+        }
+      },
+      maxBounds: {
+        _northEast: {
+          lat: 43.19065313818291,
+          lng: -87.6551432203354
+        },
+        _southWest: {
+          lat: 42.84223446764269,
+          lng: -88.28823037853854
+        }
+      },
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       markers: [],
       items: [],
-      selectedItem: null
+      selectedItem: null,
+      selectedHistoricPhoto: null
     }
   },
   methods: {
     zoomUpdated (zoom) {
       this.zoom = zoom;
-      console.log('Zoom: ' + zoom);
     },
     centerUpdated (center) {
       this.center = center;
-      console.log('Center: ' + center.lat + ', ' + center.lng);
     },
     boundsUpdated (bounds) {
+      console.log('bounds');
       console.log(bounds);
       this.bounds = bounds;
       this.refreshItems();
@@ -85,7 +115,14 @@ export default {
     onMarkerClick (itemId) {
       this.selectedItem = this.items.find(x => x.id == itemId);
     },
+    onThumbnailClick (historicPhoto) {
+      this.selectedHistoricPhoto = historicPhoto;
+      this.$bvModal.show('modalFullSizeImage');
+    },
     refreshItems () {
+      if (!this.bounds)
+        return;
+
       let url ='/api/historicPhotoLocation?includes=historicPhotos&limit=100';
 
       url += 
